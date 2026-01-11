5b7f460a8624929f00d1c5c8d12b623ccaae6ebd
lingqingmeng <lingqmeng888@gmail.com>
2026-01-11 14:37:10 +0700

    first com

---
+++ my-strapi-project/types/generated/components.d.ts
@@ -0,0 +1,34 @@
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface BlocksHero extends Struct.ComponentSchema {
+  collectionName: 'components_blocks_heroes';
+  info: {
+    displayName: 'Hero';
+  };
+  attributes: {
+    content: Schema.Attribute.Text;
+    heading: Schema.Attribute.String;
+    image: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
+    subheading: Schema.Attribute.String;
+  };
+}
+
+export interface FeatureitemFeatureitems extends Struct.ComponentSchema {
+  collectionName: 'components_featureitem_featureitems';
+  info: {
+    displayName: 'Featureitems';
+  };
+  attributes: {
+    description: Schema.Attribute.Text;
+    title: Schema.Attribute.String;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'blocks.hero': BlocksHero;
+      'featureitem.featureitems': FeatureitemFeatureitems;
+    }
+  }
+}
